1b80d20d5bf567b4c85745218d236351c2bca48c
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-09-08 22:12:07 +0530

    (feat) solution to Problem 3: largest prime factor

---
+++ 03.prime_factors.rb
@@ -0,0 +1,24 @@
+def prime_factors(num)
+  factors = (1..Math.sqrt(num).ceil).filter { |div| prime?(div) && (num % div).zero? }
+  factors.filter { |f| prime?(f) }
+end
+
+def prime?(num)
+  return false if num < 2
+
+  (Math.sqrt(num) - 1).floor.times do |i|
+    return false if (num % (i + 2)).eql?(0)
+  end
+  true
+end
+
+puts prime_factors(13_195)
+puts prime_factors(600_851_475_143)
+# puts prime_factors(7)
+# puts prime_factors(17)
+# puts prime_factors(37)
+# puts prime_factors(35)
+# puts prime_factors(45)
+
+puts prime?(35)
+puts prime?(7)
