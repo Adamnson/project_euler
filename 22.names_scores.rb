dd8416148d2ea18331b1d0b616a221180180c16d
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-09-28 22:55:50 +0530

    (feat!2) add solution 22 and helper file

---
+++ 22.names_scores.rb
@@ -0,0 +1,10 @@
+NAMES = File.read("helper.22_names.txt").split(",").map { |e| e.gsub('"', "") }.sort
+
+sum = 0
+puts NAMES[937]
+NAMES.each_with_index do |name, idx|
+  worth = name.bytes.map { |ch| ch - 64 }.inject(:+)
+  sum += worth * (idx + 1)
+end
+
+puts sum
