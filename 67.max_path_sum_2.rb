6d6d17a73931d1e5f7531dab0886c80e5e43b0e2
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-09-28 12:42:03 +0530

    (feat!1) add solution 67 based on 18

---
+++ 67.max_path_sum_2.rb
@@ -0,0 +1,8 @@
+require("./18.max_path_sum_1")
+
+NUMBERS67 = File.read("helper.67_triangle.md").split("\n")
+
+tree = Tree.new(NUMBERS67)
+sum, seq = tree.calc_sum_of_triangle(NUMBERS67.size - 2)
+puts "sum is #{sum}"
+p seq
