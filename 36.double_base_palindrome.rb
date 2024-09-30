bb0dc83a6830d14aad26e3bb06f656c986d5b593
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-09-30 23:08:32 +0530

    (BREAKING_CHANGE!3) solution 36, level 1 reached
    solved 25 problems

---
+++ 36.double_base_palindrome.rb
@@ -0,0 +1,16 @@
+def palindrome?(str)
+  str.eql?(str.reverse)
+end
+
+nums = []
+pals = []
+(1..1_000_000).each do |itr|
+  nums = [itr, itr.to_s(2)]
+  nums.append(nums.map { |n| palindrome?(n.to_s) })
+  p nums
+  pals.append(nums[0..1]) if nums[2].all?
+end
+
+p pals, pals.count
+sum = 0
+p "sum : #{pals.map { |arr| arr[0].to_i }.inject(:+)}"
