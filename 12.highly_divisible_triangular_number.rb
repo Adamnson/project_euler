f7235b07a42a5938177258d68e66573a0583c360
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-10-01 15:23:28 +0530

    (feat!2) implement prime factorization in 12

--- 12.highly_divisible_triangular_number.rb
+++ 12.highly_divisible_triangular_number.rb
@@ -1,15 +1,31 @@
 require "./helper"
+
+def get_power(divisor, p)
+  n = 0
+  pn = p
+  while (divisor % pn).eql?(0)
+    n += 1
+    pn = p**n
+  end
+  n - 1
+end
+
+def get_prime_factorization(divisor)
+  get_factors(divisor).filter { |p| prime?(p) }
+                      .map { |q| [q, get_power(divisor, q)] }
+end
+
 num_divs = [0, [0]]
 n = 1
-while n < 10_0
+while n < 1000
   tri_num = n * (n + 1) / 2
   factors = get_factors(tri_num)
-  num_divs = [n, factors] if factors.count > num_divs[1].count
+  num_divs = [n, tri_num, factors.count, get_prime_factorization(factors.max)] if factors.count > num_divs[-1].count
   n += 1
 end
 p num_divs
 
-# puts "Start"
+# puts "Brute Force"
 # n_factors = 0
 # num = 000_000
 # max = 0
