4d3793f1efe4d25c36a59539f72b7902a9cede37
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-10-13 17:19:02 +0530

    (upkeep, feat!4) refactored 41, solution and helper for 42

--- 41.pandigital_prime.rb
+++ 41.pandigital_prime.rb
@@ -1,12 +1,12 @@
 require "./helper"
 
-def get_all_pandigitals(n)
-  return 0 if n.eql?(0)
-  return 1 if n.eql?(1)
-  return nil if n > 9
+def get_all_pandigitals(size)
+  return 0 if size.eql?(0)
+  return 1 if size.eql?(1)
+  return nil if size > 9
 
   basis = %w[1 2 3 4 5 6 7 8 9]
-  basis.slice(0...n)
+  basis.slice(0...size)
        .permutation
        .map { |p| p.inject(:+).to_i }
 end
@@ -15,4 +15,4 @@ prime_pandigitals = (2..9).map do |i|
   get_all_pandigitals(i).filter(&:prime?)
 end
 
-p "ans: #{prime_pandigitals.flatten.max}"
+puts "ans: #{prime_pandigitals.flatten.max}"
---
+++ 42.coded_triangle_numbers.rb
@@ -0,0 +1,15 @@
+require "./helper"
+
+WORDS = File.read("helper.42.words.txt").split(",").map { |w| w.delete('"') }
+
+sums = WORDS.map { |w| w.bytes.map { |ch| ch - 64 }.inject(:+) }
+p WORDS[41]
+p sums[41]
+
+tiangular_numbers = (1..100).map { |n| n * (n + 1) / 2 }
+# p tiangular_numbers
+# sums.each_with_index do |sum, idx|
+#   puts "#{sum}, #{WORDS[idx]}" if tiangular_numbers.include?(sum)
+# end
+
+p sums.filter { |sum| tiangular_numbers.include?(sum) }.size
