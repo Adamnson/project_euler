279520831c828bc3d5b81d7cdb1561197cf7f078
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-10-17 13:26:56 +0530

    (feat!6) refactor 44 as per special_helper

--- 44.pentagon_numbers.rb
+++ 44.pentagon_numbers.rb
@@ -1,10 +1,10 @@
 require "./special_helper"
 
-pent_arr = (1..1000).map { |i| pentagon.call(i) }
+pent_arr = (1..1000).map(&:pentagon)
 
 j_k = pent_arr.product(pent_arr)
               .map { |pair| [pair[0] + pair[1], (pair[0] - pair[1]).abs] }
-              .map { |arr| [inv_pentagon.call(arr[0]), inv_pentagon.call(arr[1])] }
+              .map { |arr| [arr[0].pentagon_idx, arr[1].pentagon_idx] }
               .map(&:compact)
               .reject { |el| el.nil? || el.empty? } # this results in arrays which contains j or k indices
               .filter { |arr| arr.size.eql?(2) } # searching for the array which contains both j & k
