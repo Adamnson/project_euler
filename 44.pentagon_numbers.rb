4c1439984c2c5ca6565d40d0652daa0e8fd4b099
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-10-17 18:04:58 +0530

    (feat!4) solution 44

--- 44.pentagon_numbers.rb
+++ 44.pentagon_numbers.rb
@@ -1,18 +1,23 @@
 require "./special_helper"
 
-j_k = lambda { |low, high|
+sum_diff_idx = lambda { |low, high|
   (low..high).map(&:pentagon)
              .permutation(2) # .product(pent_arr)
-             .map { |pair| [pair[0] + pair[1], (pair[0] - pair[1]).abs] }
-             .map { |arr| [arr[0].pentagon_idx, arr[1].pentagon_idx] }
+             .map { |pair| [pair.inject(:+), pair.inject(:-).abs] }
+             .uniq
+             .map { |sum_diff_arr| [sum_diff_arr.first.pentagon_idx, sum_diff_arr.last.pentagon_idx] }
              .map(&:compact)
              .reject { |el| el.nil? || el.empty? } # this results in arrays which contains j or k indices
              .filter { |arr| arr.size.eql?(2) } # searching for the array which contains both j & k
 }
-
-start = 100_000
-interval_length = 2000
-while start < 150_000
-  puts " #{start} - #{start + interval_length} : #{j_k.call(start, start + interval_length)}"
+start = 1
+interval_length = 5000
+while start < 100
+  puts " #{start} - #{start + interval_length} : #{sum_diff_idx.call(start, start + interval_length)}"
   start += interval_length
 end
+
+puts "the sum is p_2395: #{2395.pentagon} and difference is p_1912: #{1912.pentagon}"
+puts "first number is #{0.5 * (2395.pentagon - 1912.pentagon)} at i #{(0.5 * (2395.pentagon - 1912.pentagon)).to_i.pentagon_idx}"
+puts "other number is #{0.5 * (2395.pentagon + 1912.pentagon)} at j #{(0.5 * (2395.pentagon + 1912.pentagon)).to_i.pentagon_idx}"
+puts "ans: #{1912.pentagon}"
