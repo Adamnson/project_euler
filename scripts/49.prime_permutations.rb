6743495996747cbcc35f1d9763aae5321ccea1de
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-10-23 23:05:12 +0530

    (feat!2) solution 49, requires refactoring

--- scripts/49.prime_permutations.rb
+++ scripts/49.prime_permutations.rb
@@ -1,19 +1,19 @@
-require "./helpers/helper"
+require_relative "../helpers/helper"
 
-# basis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-basis = [1, 4, 7, 8]
+ints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# four_digit_numbers = basis.permutation(4)
-# the_family = []
-# four_digit_numbers.each_with_index do |digits, idx|
-#   # the_family = digits.product(digits, digits, digits) if idx.eql?(530)
-#   the_family = digits.permutation if idx.eql?(530)
-#   the_family.uniq
-# end
-the_family = basis.permutation
-p the_family
+big_lambda = lambda { |basis|
+  the_family = basis.permutation
+                    .map { |arr| arr.inject { |sum, n| (10 * sum) + n } }.filter { |el| el > 1000 }
+  # p the_family
+  # p "number_of_primes: #{the_family.filter(&:prime?).count}"
+  the_family.permutation(3).map(&:sort).uniq
+            .reject { |triplet| triplet.map(&:composite?).any? }
+            .map { |triplet| [triplet.first, triplet[1] - triplet.first, triplet.last - triplet[1]] }
+            .filter { |triplet| (triplet[1].eql? triplet.last) && triplet.last.positive? }
+            .map { |triplet| puts "#{triplet.first}, diff: #{triplet.last} \nnums: [#{triplet.first} #{triplet.first + triplet.last} #{triplet.first + (2 * triplet.last)}]" }
+}
 
-the_family = the_family.map { |arr| arr.inject { |sum, n| (10 * sum) + n } }.filter { |el| el > 1000 }
-p the_family
-
-p "number_of_primes: #{the_family.filter(&:prime?).count}"
+ints.product(ints, ints, ints).each do |b|
+  big_lambda.call(b)
+end
