d194102e4bf00e10696842d36f6c7c1e1709c01f
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-10-23 23:20:28 +0530

    (feat!2) refactored 49
    the code is so beautiful that i am in awe of ruby #Ruby143

--- scripts/49.prime_permutations.rb
+++ scripts/49.prime_permutations.rb
@@ -1,19 +1,13 @@
 require_relative "../helpers/helper"
 
-ints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+ints = (0..9).to_a
 
-big_lambda = lambda { |basis|
-  the_family = basis.permutation
-                    .map { |arr| arr.inject { |sum, n| (10 * sum) + n } }.filter { |el| el > 1000 }
-  # p the_family
-  # p "number_of_primes: #{the_family.filter(&:prime?).count}"
-  the_family.permutation(3).map(&:sort).uniq
-            .reject { |triplet| triplet.map(&:composite?).any? }
-            .map { |triplet| [triplet.first, triplet[1] - triplet.first, triplet.last - triplet[1]] }
-            .filter { |triplet| (triplet[1].eql? triplet.last) && triplet.last.positive? }
-            .map { |triplet| puts "#{triplet.first}, diff: #{triplet.last} \nnums: [#{triplet.first} #{triplet.first + triplet.last} #{triplet.first + (2 * triplet.last)}]" }
-}
-
-ints.product(ints, ints, ints).each do |b|
-  big_lambda.call(b)
+ints.product(ints, ints, ints).each do |basis|
+  basis.permutation
+       .map(&:to_i).filter { |el| el > 1000 }
+       .permutation(3).map(&:sort).uniq
+       .reject { |triplet| triplet.map(&:composite?).any? }
+       .map    { |triplet| [triplet.first, triplet[1] - triplet.first, triplet.last - triplet[1]] }
+       .filter { |triplet| (triplet[1].eql? triplet.last) && triplet.last.positive? }
+       .map    { |triplet| puts "#{triplet.first}, diff: #{triplet.last} \nnums: [#{triplet.first} #{triplet.first + triplet.last} #{triplet.first + (2 * triplet.last)}]" }
 end
