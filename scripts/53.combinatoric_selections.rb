2ac9eef6c6c91c68841905ce3a7f8b46b201e7db
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-10-25 17:24:51 +0530

    (feat!2) solution 53

---
+++ scripts/53.combinatoric_selections.rb
@@ -0,0 +1,17 @@
+require_relative "../helpers/helper"
+
+class Integer
+  def C(r)
+    self.! / (r.! * (self - r).!)
+  end
+end
+
+puts 23.C(10)
+count = 0
+(1..100).each do |n|
+  (0..n).each do |r|
+    count += 1 if n.C(r) > 1_000_000
+  end
+end
+
+puts count
