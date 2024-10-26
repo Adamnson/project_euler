c28312d31c8b6ae309481f0daec5437a0c51e7d0
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-10-26 23:57:31 +0530

    (upkeep,bugfix,feat!2) add time tracking
    fix require error in 44

--- run_all.rb
+++ run_all.rb
@@ -1,30 +1,38 @@
 Dir.chdir("./scripts/")
 
 (1...10).each do |i|
-  puts "running #{i}"
-  puts `ruby 0#{i}*.rb`
+  t0 = Time.now
+  puts `ruby 0#{i}*.rb :out => out.md`
+  print "#{i} }%{ success!"
+  text = "#{Time.now - t0} \n"
+  File.write("time.md", text, mode: "a")
 end
 
-(10...20).each do |i|
-  puts "running #{i}"
-  puts `ruby #{i}*.rb`
+(10...50).each do |i|
+  next if [26, 31].include?(i)
+
+  t0 = Time.now
+  puts `ruby #{i}*.rb :out => out.md`
+  print "#{i}}%{ success!"
+  text = "#{Time.now - t0} \n"
+  File.write("time.md", text, mode: "a")
 end
 
-(20...30).each do |i|
-  unless [26].include?(i)
-    puts "running #{i}"
-    puts `ruby #{i}*.rb`
-  end
-end
+# (20...30).each do |i|
+#   unless [26].include?(i)
+#     puts "running #{i}"
+#     puts `ruby #{i}*.rb`
+#   end
+# end
 
-(30...40).each do |i|
-  unless [31].include?(i)
-    puts "running #{i}"
-    puts `ruby #{i}*.rb`
-  end
-end
+# (30...40).each do |i|
+#   unless [31].include?(i)
+#     puts "running #{i}"
+#     puts `ruby #{i}*.rb`
+#   end
+# end
 
-(40...50).each do |i|
-  puts "running #{i}"
-  puts `ruby #{i}*.rb`
-end
+# (40...50).each do |i|
+#   puts "running #{i}"
+#   puts `ruby #{i}*.rb`
+# end
