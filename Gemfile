b2bbd5f42a4d3ad5ffccf148a2e7a290695e5e9d
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-10-31 22:41:08 +0530

    (upkeep!3) rubocop compliance, update range to reduce runtime
    runtime reduced from 250+ to ~120

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ source 'https://rubygems.org'
 gem 'rubocop', '~> 1.66'
 
 gem "rainbow", "~> 3.1"
+
+gem "pry-byebug", "~> 3.10"
--- scripts/58.spiral_primes.rb
+++ scripts/58.spiral_primes.rb
@@ -1,12 +1,11 @@
 require_relative "../helpers/helper"
-#check 28 for explationation of diagnoal terms
+# check 28 for explationation of diagnoal terms
 
-#bottom right is n**2 for odd nos in 
-#bottom left is n**2 - (n-1)
-#top left is n** - 2
+# bottom right is n**2 for odd nos in
+# bottom left is n**2 - (n-1)
+# top left is n** - 2
 
-
-#GENERAL
+# GENERAL
 # seq = (1..7).filter {|n| n.odd?}
 # diagonal = []
 
@@ -14,9 +13,9 @@ require_relative "../helpers/helper"
 # puts "bottom left : #{seq.map{|el| el.pow(2) - (el-1)}}"
 # puts "top left : #{seq.map{|el| el.pow(2) - 2*(el-1)}}"
 # puts "top right : #{seq.map{|el| el.pow(2) - 3*(el-1)}}"
-#GENERAL
+# GENERAL
 
-#one
+# one
 # ratio = 1.0
 # n = 2000
 # while n<5000
@@ -31,32 +30,31 @@ require_relative "../helpers/helper"
 #   #puts "diag : #{diagonal}"
 #   #puts "[ #{diagonal.filter(&:prime?).size.to_f} ,#{ diagonal.size}]"
 #   ratio = diagonal.filter(&:prime?).size.to_f / diagonal.size
-#   p [n, ratio] 
+#   p [n, ratio]
 #   break if ratio < 0.1
 #   n +=1
-  
+
 # end
 
-#[26590, 0.1000056415367546]
-#[26591, 0.09999811962919088]
+# [26590, 0.1000056415367546]
+# [26591, 0.09999811962919088]
 
-#TWO UNCOMMENT THIS =BEGIN
-populate_diagonal = lambda {|limit|
+# TWO UNCOMMENT THIS =BEGIN
+populate_diagonal = lambda { |limit|
   arr = []
   seq = (1..limit).filter(&:odd?)
-  arr.append(seq.map{|el| el.pow(2)})
-  arr.append(seq.map{|el| el.pow(2) - (el-1)})
-  arr.append(seq.map{|el| el.pow(2) - 2*(el-1)})
-  arr.append(seq.map{|el| el.pow(2) - 3*(el-1)})
+  arr.append(seq.map { |el| el.pow(2) })
+  arr.append(seq.map { |el| el.pow(2) - (el - 1) })
+  arr.append(seq.map { |el| el.pow(2) - (2 * (el - 1)) })
+  arr.append(seq.map { |el| el.pow(2) - (3 * (el - 1)) })
   arr.flatten.uniq!
 }
 
-t0=Time.now
-(26200..26300).to_a.filter(&:odd?).map {|num| [num, populate_diagonal.call(num)]}
-.map {|el| [el.first, el.last.filter(&:prime?).size, el.last.size,]}
-.map {|arr| arr.append(arr[1].to_f/arr.last)}
-.filter {|el| el.last < 0.1}
-.map {|el| p el} 
-puts Time.now-t0
-#UNTIL HERE =END
-
+t0 = Time.now
+(26_200..26_250).to_a.filter(&:odd?).map { |num| [num, populate_diagonal.call(num)] }
+                .map { |el| [el.first, el.last.filter(&:prime?).size, el.last.size] }
+                .map { |arr| arr.append(arr[1].to_f / arr.last) }
+                .filter { |el| el.last < 0.1 }
+                .map { |el| p el }
+puts Time.now - t0
+# UNTIL HERE =END
