4a1095cf4e03887cf3d8fe8b80e0593ce3e8d2a3
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-12-27 04:54:47 +0530

    (feat!5) fixed partial pattern capture
    added a bracket crawler condition to detect bracket mismatch and correct
    it by concatination with next element

--- scripts/910.l_expressions_2_v2.rb
+++ scripts/910.l_expressions_2_v2.rb
@@ -10,10 +10,22 @@ class L_Exp
     # regex : /S(\(.{1,}\)){3}/g
     puts "Transform S"
     pattern = /S(\(.{1,}\)){3}/.match exp
+    # pattern = /S((\()(.+)(\))){3}/.match exp
     pattern_start_idx = exp.index(pattern.to_s)
     # puts exp.scan(/S(\(.{1,}\)){3}/pattern)
     l_exp = exp.split(")(")
-    puts l_exp.inspect, l_exp.size
+    puts "before:[#{l_exp.inspect}], [#{l_exp.size}]"
+    l_exp.each_with_index do |sub_pattern, idx|
+      next if (sub_pattern == l_exp.first) or (sub_pattern == l_exp.last)
+
+      next unless sub_pattern.count("(") != sub_pattern.count(")")
+
+      puts "editing #{sub_pattern} and #{l_exp[idx + 1]}"
+      l_exp[idx] += l_exp[idx + 1]
+      l_exp[idx + 1] = ""
+    end
+    l_exp.reject! { |el| el.eql?("") }
+    puts "after:[#{l_exp.inspect}], [#{l_exp.size}]"
     puts "calling s function on #{pattern} at #{pattern_start_idx}"
     third_param_last_idx_correction = l_exp.size == 3 ? -2 : -1
     u = l_exp.first[2..]
@@ -22,6 +34,9 @@ class L_Exp
     coloured_u = Rainbow(u).color(:forestgreen)
     coloured_v = Rainbow(v).color(:orangered)
     coloured_w = Rainbow(w).color(:darkorange)
+    if (u.count("(") != u.count(")")) or (v.count("(") != v.count(")")) or (w.count("(") != w.count(")"))
+      puts Rainbow("Found bracket count mismatch : ").color(:red)
+    end
     puts("u: #{coloured_u}\nv: #{coloured_v}\nw: #{coloured_w}")
     puts "#{S(coloured_u, coloured_v, coloured_w)}#{l_exp.size == 3 ? '' : l_exp[3..].join(')(')}"
     "#{S(u, v, w)}#{l_exp.size == 3 ? '' : "(#{l_exp[3..].join(')(')}"}"
@@ -48,4 +63,8 @@ puts sza0.value
 
 s5za0 = L_Exp.new "S(S)(S(S))(S(Z))(A)(O)"
 
-puts "using return value #{s5za0.apply_s}"
+after_step_1 = L_Exp.new(s5za0.apply_s)
+puts "using return value #{after_step_1.value}"
+
+after_step_2 = L_Exp.new(after_step_1.apply_s)
+puts "using return value #{after_step_2.value}"
