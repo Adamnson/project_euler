afbe57d219a322d5ef71968ee0b34689dddd0941
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-03-13 18:16:57 +0530

    (test!6) added tests for 26.v2

--- spec/26.v2.spec_helper.rb
+++ spec/26.v2.spec_helper.rb
@@ -25,4 +25,21 @@ describe Reciprocal do
       expect(rec.denominator).to eql("3"*100)
     end   
   end
+
+  describe "check truncate_zeros" do
+    it "truncates trailing zeros in terminating denominators" do
+      rec = Reciprocal.new(10)
+      rec.find_denominator
+      expect(rec.num).to eql(10)
+      expect(rec.denominator).to eql("1")
+      expect(rec.denominator).not_to eql("1" + "0"*99)
+    end
+
+    it "raises error if denominator string is empty" do
+      rec = Reciprocal.new(8)
+      rec.truncate_zeros
+      expect(rec.num).to eql(8)
+      expect(rec.denominator).to eql("")
+    end
+  end
 end
