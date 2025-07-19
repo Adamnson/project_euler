404787deb2ec87483ef6826845d7ef466aa6a8e4
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-07-19 18:00:57 +0530

    (test) add tests for #format

--- spec/910.v3.expression_spec_helper.rb
+++ spec/910.v3.expression_spec_helper.rb
@@ -1,11 +1,30 @@
 require_relative "../scripts/910.l_expressions_2_v3"
 
 describe Expression do
-  describe "initialization" do
+  describe "#initialize" do
     it "creates a new expression, identifies start" do
       e = Expression.new("A(0)")
       expect(e.value).to eql("A(0)")
       expect(e.queue).to be_empty
+      expect(e.tail).to be_empty
+    end
+  end
+
+  describe "#format" do
+    it "updates @node" do
+      e = Expression.new("A(0)")
+      expect(e.value).to eql("A(0)")
+      expect(e.queue).to be_empty
+      expect(e.tail).to be_empty
+      expect { e.format }.to change(e, :node).from(nil)
+    end
+
+    it "updates @value" do
+      e = Expression.new("A(0)")
+      expect(e.value).to eql("A(0)")
+      expect(e.queue).to be_empty
+      expect(e.tail).to be_empty
+      expect { e.format }.to change(e, :value).from("A(0)").to(1)
     end
   end
 end
