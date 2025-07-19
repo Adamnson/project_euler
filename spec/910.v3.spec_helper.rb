06425acedf02d31fcbbc2ea2ba9302db20fcc171
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-07-19 14:10:33 +0530

    (test) added tests for Node#transform

--- spec/910.v3.spec_helper.rb
+++ spec/910.v3.spec_helper.rb
@@ -4,26 +4,49 @@ describe Node do
   describe "automatic operator assignment" do
     it "creates an A expression when only one parameter is supplied" do
       n = Node.new(5)
-      expect(n.params).to eql({:u=>5})
+      expect(n.params).to eql({ u: 5 })
       expect(n.operator).to eql("A")
     end
-   
+
     it "creates a Z expression when two parameters are supplied" do
-      n = Node.new(7,11)
-      expect(n.params).to eql({:u=>7,:v=>11})
+      n = Node.new(7, 11)
+      expect(n.params).to eql({ u: 7, v: 11 })
       expect(n.operator).to eql("Z")
     end
 
     it "creates a S expression when three parametes are supplied" do
-      n = Node.new(7,2,6)
-      expect(n.params).to eql({:u=>7,:v=>2,:w=>6})
+      n = Node.new(7, 2, 6)
+      expect(n.params).to eql({ u: 7, v: 2, w: 6 })
       expect(n.operator).to eql("S")
     end
 
     it "creates an A(0) expression when no parameters are supplied" do
       n = Node.new
-      expect(n.params).to eql({:u=>0})
+      expect(n.params).to eql({ u: 0 })
       expect(n.operator).to eql("A")
+    end
+  end
+
+  describe "check transformation for A operator" do
+    it "increments the value by 1 if the parameter is a number (PASSED AS STRING), to_queue flag is false" do
+      output_hashes = (0..10_000).to_a.map { |i| Node.new(i.to_s) } # need to pass strings NOT INTEGERS
+                                 .map(&:transform)
+      output_hashes.map { |out| expect(out).to have_key(:new_expression) }
+      output_hashes.map { |out| expect(out).to have_key(:to_queue) }
+      output_hashes.map { |out| expect(out).not_to have_key(:for_queue) }
+      expect(output_hashes).to match_array((1..10_001).to_a.map { |n| { new_expression: n, to_queue: false } })
+    end
+
+    it "passes the parameter to create a new expression, adds 'A' to queue, to_queue flag is true" do
+      test_expressions = ["A(0)", "Z(S)(0)", "S(1)(Z)(2)"]
+      test_expressions.each do |exp|
+        n = Node.new(exp)
+        output_hash = n.transform
+        expect(output_hash).to have_key(:new_expression)
+        expect(output_hash).to have_key(:to_queue)
+        expect(output_hash).to have_key(:for_queue)
+        expect(output_hash).to eql({ new_expression: exp, to_queue: true, for_queue: "A" })
+      end
     end
   end
 end
